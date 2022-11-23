591beb08e78affa79221593dfc8ae72baf7049c7
Sami Güntepe <sami@Sami-MacBook-Air.local>
2022-11-23 15:35:59 +0300

    0.5

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -6,7 +6,7 @@
 /*   By: sguntepe <@student.42kocaeli.com.tr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/20 17:57:10 by sguntepe          #+#    #+#             */
-/*   Updated: 2022/11/22 15:26:17 by sguntepe         ###   ########.fr       */
+/*   Updated: 2022/11/23 15:34:23 by sguntepe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,24 +36,24 @@ void	ft_lstiter(t_list *lst, void (*f)(void *))
 
 // int main()
 // {
-//     t_list *node1,*node2,*node3;
+// 	  t_list *node1,*node2,*node3;
 
-//     node1 = (t_list *)malloc(sizeof(t_list));
-//     node2 = (t_list *)malloc(sizeof(t_list));
-//     node3 = (t_list *)malloc(sizeof(t_list));
+// 	  node1 = (t_list *)malloc(sizeof(t_list));
+// 	  node2 = (t_list *)malloc(sizeof(t_list));
+// 	  node3 = (t_list *)malloc(sizeof(t_list));
 
-// 	ft_lstadd_back(&node1,node2);
-// 	ft_lstadd_back(&node1,node3);
+// 	  ft_lstadd_back(&node1,node2);
+// 	  ft_lstadd_back(&node1,node3);
 
-//     node1->content = ft_strdup("kocaeli");
-//     node2->content = ft_strdup("kocaeli");
-//     node3->content = ft_strdup("kocaeli");
+// 	  node1->content = ft_strdup("kocaeli");
+// 	  node2->content = ft_strdup("kocaeli");
+// 	  node3->content = ft_strdup("kocaeli");
 
-//     ft_lstiter(node1,ft_change);
+// 	  ft_lstiter(node1,ft_change);
 
-//     printf("%s\n",(char *)node1->content);
-//     printf("%s\n",(char *)node1->next->content);
-//     printf("%s\n",(char *)node1->next->next->content);
+// 	  printf("%s\n",(char *)node1->content);
+// 	  printf("%s\n",(char *)node1->next->content);
+// 	  printf("%s\n",(char *)node1->next->next->content);
 // }
 
 // struct bağlı listelerinin her bir elemanının contentine parametredeki fonksiyonu uygular.
--- ft_lstlast.c
+++ ft_lstlast.c
@@ -6,7 +6,7 @@
 /*   By: sguntepe <@student.42kocaeli.com.tr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/20 15:00:54 by sguntepe          #+#    #+#             */
-/*   Updated: 2022/11/20 16:18:35 by sguntepe         ###   ########.fr       */
+/*   Updated: 2022/11/23 15:32:35 by sguntepe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,14 +27,16 @@ t_list	*ft_lstlast(t_list *lst)
 // {
 //  t_list *node1,*node2,*node3;
 
-//  node1 = (t_list *)malloc(sizeof(t_list));
+// 	node1 = (t_list *)malloc(sizeof(t_list));
 // 	node2 = (t_list *)malloc(sizeof(t_list));
 // 	node3 = (t_list *)malloc(sizeof(t_list));
 
-//  node1->next = node2;
+// 	node1->next = node2;
 // 	node2->next = node3;
 // 	node3->next = NULL;
-//  node3->content = "42Kocaeli";
+// 	node3->content = "42Kocaeli";
 
 //  printf("%s",(char *)ft_lstlast(node1)->content);
 // }
+
+// lst struct yapısının bağlı olduğu son struct yapısını döndürür.
