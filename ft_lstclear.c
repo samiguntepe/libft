035782a09a1bf0c9fc9d51acc8f4032561b0a078
Sami Güntepe <sami@Sami-MacBook-Air.local>
2022-11-23 15:25:56 +0300

    0.4

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,12 +6,12 @@
 /*   By: sguntepe <@student.42kocaeli.com.tr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/20 15:00:48 by sguntepe          #+#    #+#             */
-/*   Updated: 2022/11/22 15:52:56 by sguntepe         ###   ########.fr       */
+/*   Updated: 2022/11/23 15:24:56 by sguntepe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
+// #include <stdio.h>
 void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
 	t_list	*temp;
@@ -26,30 +26,39 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 	}
 }
 
-void    *del(void *lst)
-{
-    t_list *ptr;
+// void    *del(void *content)
+// {
+//    free(content);
+//     return 0;
+// }
 
-    ptr = (t_list *)lst;
+// int main(void)
+// {
+//     t_list *node1,*node2,*node3;
+	
+// 	node1 = (t_list *)malloc(sizeof(t_list));
+//     node2 = (t_list *)malloc(sizeof(t_list));
+// 	node3 = (t_list *)malloc(sizeof(t_list));
+	
+//     node1->content = ft_strdup("42Kocaeli");
+// 	node2->content = ft_strdup("42Kocaeli");
+// 	node3->content = ft_strdup("42Kocaeli");
 
-    ptr->content = 0;
-    return 0;
-}
+// 	ft_lstadd_back(&node1,node2);
+// 	ft_lstadd_back(&node1,node3);
 
-int main(void)
-{
-    t_list *node1,*node2,*node3;
+// 	ft_lstclear(&node2,(void *)(*del));
 
-    node1 = ft_lstnew("42Kocaeli");
-	node2 = ft_lstnew("42Kocaeli");
-	node3 = ft_lstnew("42Kocaeli");
+// 	if(node2)
+// 		printf("1\n");
+// 	else
+// 		printf("0\n");
 
-	ft_lstadd_back(&node1,node2);
-	ft_lstadd_back(&node1,node3);
+// 	if(node1)
+// 		printf("1\n");
+// 	else
+// 		printf("0\n");
+// }
 
-	ft_lstclear(&node1,del);
-
-	printf("%s",node1->content);
-}
-
-// 
+// lst struct yapısının bağlı olduğu tüm içerikleri ve yapıları temizler.
+// lst struct'ına bağlı olan struct yapılarını temizlemez.
